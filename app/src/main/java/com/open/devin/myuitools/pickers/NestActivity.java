f54a378b0e50a8c464204b90a432ea7693851c8c
devinc <873296590@qq.com>
2018-11-02 17:26:01 +0800

    enhance them

--- app/src/main/java/com/open/devin/myuitools/pickers/NestActivity.java
+++ app/src/main/java/com/open/devin/myuitools/pickers/NestActivity.java
@@ -1,22 +1,16 @@
 package com.open.devin.myuitools.pickers;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 
 import com.open.devin.myuitools.R;
 
-public class PickersActivity extends AppCompatActivity {
+public class NestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pickers);
-    }
-
-    public void onNestView(View view) {
-        startActivity(new Intent(this, NestActivity.class));
+        setContentView(R.layout.activity_nest);
     }
 }
