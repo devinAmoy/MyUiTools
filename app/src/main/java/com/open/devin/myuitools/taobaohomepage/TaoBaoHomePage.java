38d63b5868924cac859ce6aff28eecaaef480bdb
devinchen <873296590@qq.com>
2018-11-21 23:05:02 +0800

    init taobao home page

---
+++ app/src/main/java/com/open/devin/myuitools/taobaohomepage/TaoBaoHomePage.java
@@ -0,0 +1,15 @@
+package com.open.devin.myuitools.taobaohomepage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.open.devin.myuitools.R;
+
+public class TaoBaoHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tao_bao_home_page);
+    }
+}
