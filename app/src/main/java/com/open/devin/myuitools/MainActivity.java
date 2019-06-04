49f0fac1d21b53e7724cc8c7e6dcbcd74eb64670
devinc <873296590@qq.com>
2019-06-04 15:05:59 +0800

    delete picker

--- app/src/main/java/com/open/devin/myuitools/MainActivity.java
+++ app/src/main/java/com/open/devin/myuitools/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.open.devin.myuitools.pickers.PickersActivity;
 import com.open.devin.myuitools.taobaohomepage.TaoBaoHomePageActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startModule(R.id.picker, PickersActivity.class);
         startModule(R.id.tao_bao, TaoBaoHomePageActivity.class);
     }
 
