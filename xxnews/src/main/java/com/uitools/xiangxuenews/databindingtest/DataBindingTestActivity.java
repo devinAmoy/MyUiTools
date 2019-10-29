655bdcb91a065f9f9338cebb113a32dffbf5a812
devinc <873296590@qq.com>
2019-10-29 10:04:40 +0800

    rename module

--- xxnews/src/main/java/com/uitools/xiangxuenews/databindingtest/DataBindingTestActivity.java
+++ xxnews/src/main/java/com/uitools/xiangxuenews/databindingtest/DataBindingTestActivity.java
@@ -2,7 +2,6 @@ package com.uitools.xiangxuenews.databindingtest;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,12 +21,11 @@ public class DataBindingTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityDataBindingTestBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding_test);
         final User user = new User("aaa", "bbb");
-        binding.setUser(null);
-        final EditText editText = binding.getRoot().findViewById(R.id.first_name_edt);
-        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        binding.setUser(user);
+        binding.firstNameEdt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                user.setFirstName(editText.getText().toString());
+                user.setFirstName(v.getText().toString());
                 return false;
             }
         });
