60c88270075a682a9ffa69a973ab050345b07660
devinc <873296590@qq.com>
2019-10-30 14:55:00 +0800

    slide options

---
+++ androidbus/src/main/java/com/example/slidingoptions/SlidingOptionsActivity.java
@@ -0,0 +1,16 @@
+package com.example.slidingoptions;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.leftsidemenu.R;
+
+public class SlidingOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliding_options);
+    }
+}
---
+++ androidbus/src/main/java/com/example/slidingoptions/SlidingOptionsView.java
@@ -0,0 +1,21 @@
+package com.example.slidingoptions;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class SlidingOptionsView extends View {
+
+    public SlidingOptionsView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
