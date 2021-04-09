f2f60516fc43e6d703d6a7ed5c23560de029bb60
devinc <873296590@qq.com>
2021-04-09 09:13:29 +0800

    add kotlin

---
+++ androidbus/src/main/java/com/example/huadongdingbu/HuaDongDingBuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.huadongdingbu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.leftsidemenu.R
+
+class HuaDongDingBuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hua_dong_ding_bu)
+    }
+}
